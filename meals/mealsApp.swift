7dc90939c7d06b065098e0702cb70ea8195fa40e
ProgramComputer <22284856+ProgramComputer@users.noreply.github.com>
2023-11-13 19:14:41 -0600

    Initial Commit

---
+++ meals/mealsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mealsApp.swift
+//  meals
+//
+//  Created by Paul C on 11/13/23.
+//
+
+import SwiftUI
+
+@main
+struct mealsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
